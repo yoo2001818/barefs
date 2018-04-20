347ba67e62e47314fd09ab7a5d20d7b09d8cd866
Duknam Yoo <yoo2001818@gmail.com>
2018-04-21 06:05:20 +0900

    Implement block manager

--- src/blockManager.ts
+++ src/blockManager.ts
@@ -10,19 +10,27 @@ export default class BlockManager {
   constructor(fs: FileSystem) {
     this.fs = fs;
   }
-
   init(bitmapFile: File): void {
     this.bitmapFile = bitmapFile;
   }
-
   async getType(id: number): Promise<BlockType> {
-    return 0;
+    // TODO Optimize it using cache....
+    return (await this.bitmapFile.read(id, 1))[0];
   }
-
   async setType(id: number, value: BlockType): Promise<void> {
+    await this.bitmapFile.write(id, new Uint8Array([value]));
   }
-
   async next(): Promise<number> {
-    return 1;
+    // TODO Again, optimize it using cache....
+    let position = 0;
+    while (position < this.bitmapFile.length) {
+      let block = await this.bitmapFile.read(position,
+        Math.min(4096, this.bitmapFile.length - position));
+      for (let i = 0; i < block.length; ++i) {
+        if (block[i] === 0) return position + i;
+      }
+      position += 4096;
+    }
+    return this.bitmapFile.length;
   }
 }
--- src/inodeManager.ts
+++ src/inodeManager.ts
@@ -11,22 +11,18 @@ export default class INodeManager {
   constructor(fs: FileSystem) {
     this.fs = fs;
   }
-
   init(blockListFile: File): void {
     this.blockListFile = blockListFile;
   }
-
   async read(id: number): Promise<INode> {
     // TODO Get offset from block list!!!
     return decode(id, await this.fs.diskDriver.read(id * 128, 128));
   }
-
   async write(id: number, inode: INode): Promise<void> {
     // TODO Get offset from block list!!!
     // Also, if the block is full, allocate new block
     return this.fs.diskDriver.write(id * 128, encode(inode));
   }
-
   async next(): Promise<INode> {
     // TODO Since all blocks are placed on the bitmap, get the first inode block
     // with empty node.
