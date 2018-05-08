186790a53c9c03514e1febade0bac0d9f1def859
Duknam Yoo <yoo2001818@gmail.com>
2018-05-09 01:30:41 +0900

    Fix inode manager resolving

--- src/inodeManager.ts
+++ src/inodeManager.ts
@@ -7,6 +7,7 @@ export type BlockType = number;
 
 // TODO handle constants properly
 const INODE_COUNT = 4096 / 128;
+const BLOCK_CHUNK = 12 * (4096 / 12 | 0);
 export default class INodeManager {
   fs: FileSystem;
   blockListFile: File;
@@ -19,38 +20,41 @@ export default class INodeManager {
   }
   async getBlockId(id: number): Promise<number> {
     if (this.blockListFile == null || this.blockListFile.length === 0) {
-      return id;
+      return 0;
     }
     // Translate inode ID to block ID
     let blockPos = Math.floor(id / INODE_COUNT);
     return createDataView(
-      await this.blockListFile.read(16 * blockPos + 4, 4)).getUint32(0);
+      await this.blockListFile.read(12 * blockPos + 4, 4)).getUint32(0);
   }
   async read(id: number): Promise<INode> {
     let blockId = await this.getBlockId(id);
+    let addr = blockId * FileSystem.BLOCK_SIZE +
+      (id % INODE_COUNT) * FileSystem.INODE_SIZE;
     return decode(id,
-      await this.fs.diskDriver.read((id % INODE_COUNT) * FileSystem.INODE_SIZE,
-      FileSystem.INODE_SIZE));
+      await this.fs.diskDriver.read(addr, FileSystem.INODE_SIZE));
   }
   async write(id: number, inode: INode): Promise<void> {
     let blockId = await this.getBlockId(id);
-    await this.fs.diskDriver.write((id % INODE_COUNT) * FileSystem.INODE_SIZE,
-      encode(inode));
+    let addr = blockId * FileSystem.BLOCK_SIZE +
+      (id % INODE_COUNT) * FileSystem.INODE_SIZE;
+    await this.fs.diskDriver.write(addr, encode(inode));
   }
   async next(): Promise<INode> {
     // TODO Optimize it using cache....
     let position = 0;
     let found = false;
     let result = this.blockListFile.length / 12 * INODE_COUNT;
-    while (position * 4096 < this.blockListFile.length) {
-      let block = createDataView(await this.blockListFile.read(position * 4096,
-        Math.min(4096, this.blockListFile.length - position * 4096)));
+    while (position * BLOCK_CHUNK < this.blockListFile.length) {
+      let block = createDataView(await this.blockListFile.read(
+        position * BLOCK_CHUNK, Math.min(BLOCK_CHUNK,
+          this.blockListFile.length - position * BLOCK_CHUNK)));
       for (let i = 0; i < block.byteLength; i += 12) {
         let bitset = block.getUint32(i + 8) | 0;
         if (bitset !== (0xFFFFFFFF | 0)) {
           for (let j = 0; j < 32; ++j) {
             if ((bitset & (1 << j)) === 0) {
-              result = position * INODE_COUNT + j;
+              result = ((position * BLOCK_CHUNK + i) / 12) * INODE_COUNT + j;
               found = true;
               break;
             }
@@ -74,11 +78,11 @@ export default class INodeManager {
     } else {
       bitsetView = createDataView(new Uint8Array(12));
       bitset = 0;
-      await this.fs.setType(blockPos, 1);
+      let blockId = await this.fs.blockManager.next(1);
+      bitsetView.setUint32(4, blockId);
     }
     bitset |= 1 << (inode.id % INODE_COUNT);
     bitsetView.setUint32(8, bitset);
-    console.log('AAAAAAA', 12 * blockPos, createUint8Array(bitsetView));
     await this.blockListFile.write(12 * blockPos, createUint8Array(bitsetView));
     return inode;
   }
